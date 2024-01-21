ae5e69accf3f758fa758b5507f3338920a31749e
diegohuertan <124300301+diegohuertan@users.noreply.github.com>
2024-01-21 14:41:38 -0300

    evitar sleep de backend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRoutes } from "react-router";
 import Router from "./routes/Router";
+import axios from 'axios';
 import "./index.css";
 
 function App() {
   const routing = useRoutes(Router);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      axios.get('https://dhermesback.onrender.com/api/medicamentos')
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
+    }, 25 * 60 * 1000); // Cada 25 minutos
+
+    return () => clearInterval(interval); // Limpia el intervalo al desmontar el componente
+  }, []);
+
   return (
     <React.Fragment>
       {routing}
